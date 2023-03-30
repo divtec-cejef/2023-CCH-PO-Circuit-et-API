93f6343d3aa221b8e7bfda8475306ce279476cf5
dindin-nibnib <mail@dindin.ch>
2023-03-30 17:18:30 +0200

    can get all cars or cars by id

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,35 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const app = express();
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
+});
+
+app.get('/car', async (req, res) => {
+	console.log(req);
+	let { id } = req.query;
+
+	if (!id) {
+		let found = await prisma.voiture.findMany();
+		res.send(found.map((car) => car.pseudo));
+		return;
+	}
+
+	if (typeof id !== 'string') {
+		res.send('Error');
+		return;
+	}
+
+	let found = await prisma.voiture.findFirst({
+		where: {
+			url: id
+		}
+	});
+	res.send(found?.pseudo);
 });
 
 app.listen(3001, () => {
