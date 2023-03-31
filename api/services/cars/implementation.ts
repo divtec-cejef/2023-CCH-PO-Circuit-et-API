d7c72328e0ee73ea523f3fe082c7df47ce3fc115
dindin-nibnib <mail@dindin.ch>
2023-03-31 12:46:07 +0200

    finished car read implementation

--- api/services/cars/implementation.ts
+++ api/services/cars/implementation.ts
@@ -1,33 +1,44 @@
-import {PrismaClient} from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 export const getCars = () => {
-    return true
-}
+	return true;
+};
 
-type IdVoiture = string | number
+type IdVoiture = string | number;
 
 export const getCar = async (id_voiture: IdVoiture) => {
-    if (typeof id_voiture == 'string') {
-        return await getCarWithString(id_voiture)
-    } else {
-        return await getCarWithNumber(id_voiture);
-    }
-}
+	if (typeof id_voiture == 'string')
+		return await getCarWithString(id_voiture);
+	else
+		return await getCarWithNumber(id_voiture);
+};
 
-async function getCarWithString(idVoiture:string) {
-    const car = await prisma.
-}
+const getCarWithString = async (idVoiture: string) => {
+	const car = await prisma.voiture.findFirst({
+		where: {
+			url: idVoiture
+		}
+	});
 
-async function getCarWithNumber(idVoiture:number) {
+	return car;
+};
 
+async function getCarWithNumber(idVoiture: number) {
+	const car = await prisma.voiture.findFirst({
+		where: {
+			id_voiture: idVoiture
+		}
+	});
+
+	return car;
 }
 
 export const updateCar = (id_voiture: number) => {
 
-}
+};
 
 export const deleteCar = (id_voiture: number) => {
 
-}
+};
