71ee317894e6179640c8ee813fda2570904d311e
dindin-nibnib <mail@dindin.ch>
2023-04-27 09:08:52 +0200

    better .env

--- qrcode-generator/index.ts
+++ qrcode-generator/index.ts
@@ -1,14 +1,19 @@
 import fs from 'fs';
 import { PrismaClient } from "@prisma/client";
 import QRCode from "qrcode";
+import dotenv from "dotenv";
 
 const prisma = new PrismaClient();
-
+dotenv.config();
+const frontRoute = process.env.FRONT_ROUTE;
 
 prisma.car.findMany().then((cars) => {
 	cars.forEach(async (car) => {
 		try {
-			fs.writeFileSync(`out/${car.pseudo}.png`, await QRCode.toBuffer(`http://localhost:3000/${car.query_id}`));
+			if (!car.query_id) return;
+			const carRoute = frontRoute?.replace('${1}', car.query_id);
+			if (!carRoute) return;
+			fs.writeFileSync(`out/${car.pseudo}.png`, await QRCode.toBuffer(carRoute));
 		} catch (err) {
 			console.error(err);
 		}
