00e9fc095808dc041a11fda2e4cf35a33fb74189
chl0e-rnd <94692954+chl0e-rnd@users.noreply.github.com>
2023-04-27 09:18:12 +0200

    Changement de volant.png

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import home from "@/views/HomeView.vue";
+import HomeView from '../views/UserView.vue'
+import home from "@/views/UserView.vue";
 import ScanView from "@/views/ScanView.vue";
 import HistoryView from "@/views/HistoryView.vue";
 import AboutView from "@/views/AboutView.vue";
