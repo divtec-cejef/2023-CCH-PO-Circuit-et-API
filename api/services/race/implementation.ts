129e4d4b893dbbff5b3147ed31481848ad201b3f
Tobias Glauser <tobiasglauser2006@gmail.com>
2023-04-28 11:01:52 +0200

    Added getRacesByCar in implementation.ts and created route get races/:slug

---
+++ api/services/race/implementation.ts
@@ -0,0 +1,22 @@
+import buildClient from "../client";
+
+const prisma = buildClient();
+
+/**
+ * Retourne les manches d'une course d'une voiture donnée
+ * @param id id de la voiture
+ * @returns un tableau des manches
+ */
+export const getRacesByCar = async (id: number) => {
+    return await prisma.race.findMany({
+        where: {
+            id_car: id
+        },
+        select: {
+            id_race: true,
+            realisation_date_time: true,
+            sector_one: true,
+            id_car: true,
+        }
+    });
+}
