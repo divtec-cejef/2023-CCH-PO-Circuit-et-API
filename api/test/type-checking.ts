d206087f9db531f09710dd397482aff5e317929f
dindin-nibnib <mail@dindin.ch>
2023-04-29 23:08:25 +0200

    renamed the function

--- api/test/type-checking.ts
+++ api/test/type-checking.ts
@@ -1,10 +1,10 @@
-export const compareTypes = (a: any, struct: any, path?: string): void => {
+export const compareStructures = (a: any, struct: any, path?: string): void => {
 	for (const key in a) {
 		if (struct[key] === undefined) {
 			throw Error(`Key ${path ? `${path}.${key}` : key} is not in the interface`);
 		}
 		if (typeof a[key] === 'object') {
-			return compareTypes(a[key], struct[key], path ? `${path}.${key}` : key);
+			return compareStructures(a[key], struct[key], path ? `${path}.${key}` : key);
 		}
 		const nativeTypes = ['string', 'number', 'boolean', 'undefined', 'function'];
 		if (nativeTypes.includes(struct[key].name.toLowerCase())) {
@@ -22,7 +22,7 @@ export default function chaiStructure(chai: any, utils: any) {
 	utils.addMethod(Assertion.prototype, 'structure', function (this: any, interfaceMap: any) {
 		// map is an object map with property names as keys and strings for
 		// typeof checks or a nested interfaceMap
-		compareTypes(this._obj, interfaceMap);
+		compareStructures(this._obj, interfaceMap);
 	});
 
 }
