d43c30e86386da6e82719c9b2e85243aa35e40f5
dindin-nibnib <mail@dindin.ch>
2023-04-29 23:40:56 +0200

    now using my own package on npm

--- api/test/type-checking.ts
+++ api/test/type-checking.ts
@@ -1,20 +1,4 @@
-export const compareStructures = (a: any, struct: any, path?: string): void => {
-	for (const key in a) {
-		if (struct[key] === undefined) {
-			throw Error(`Key ${path ? `${path}.${key}` : key} is not in the interface`);
-		}
-		if (typeof a[key] === 'object') {
-			return compareStructures(a[key], struct[key], path ? `${path}.${key}` : key);
-		}
-		const nativeTypes = ['string', 'number', 'boolean', 'undefined', 'function'];
-		if (nativeTypes.includes(struct[key].name.toLowerCase())) {
-			if (typeof a[key] !== struct[key].name.toLowerCase()) {
-				throw Error(`Key ${path ? `${path}.${key}` : key} is not of type ${struct[key].name.toLowerCase()}`);
-			}
-		}
-	}
-	return;
-};
+import { checkStructuresOrThrow } from "check-structure";
 
 export default function chaiStructure(chai: any, utils: any) {
 	var Assertion = chai.Assertion;
@@ -22,7 +6,7 @@ export default function chaiStructure(chai: any, utils: any) {
 	utils.addMethod(Assertion.prototype, 'structure', function (this: any, interfaceMap: any) {
 		// map is an object map with property names as keys and strings for
 		// typeof checks or a nested interfaceMap
-		compareStructures(this._obj, interfaceMap);
+		checkStructuresOrThrow(this._obj, interfaceMap);
 	});
 
 }
