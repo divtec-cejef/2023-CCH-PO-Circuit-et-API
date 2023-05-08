dc1543870e5982165e2745493dd86783e34d9174
dindin-nibnib <mail@dindin.ch>
2023-05-08 22:15:03 +0200

    fixed multiple prisma client instanciation

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,12 @@
-import expressServer from "./express";
-import sioServer from "./socketio";
+import expressServer from "./clients/express";
+import sioServer from "./clients/socketio";
+import prisma from "./clients/prismadb";
 import http from "http";
 
 const server = http.createServer(expressServer);
 sioServer.attach(server);
 expressServer.set("socketio", sioServer);
+expressServer.set("prismadb", prisma);
 
 server.listen(3000, () => {
 	console.log("listening on *:3000");
