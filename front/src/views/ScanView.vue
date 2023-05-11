282e00409ebf37a62c0ccd6daef6fd7b48f87562
chl0e-rnd <94692954+chl0e-rnd@users.noreply.github.com>
2023-05-11 09:57:53 +0200

    Changement

--- front/src/views/ScanView.vue
+++ front/src/views/ScanView.vue
@@ -13,7 +13,7 @@
 </template>
 
 <script setup lang="ts">
-import {computed, ref} from "vue";
+import {ref} from "vue";
 import { QrcodeStream } from 'vue-qrcode-reader'
 import api from "../models/api";
 
