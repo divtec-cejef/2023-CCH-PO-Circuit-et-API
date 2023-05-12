39b71ba5ac621017eb0bc50950e92f56607a141a
Tobias Glauser <tobiasglauser2006@gmail.com>
2023-05-12 09:30:19 +0200

    Adaptation des tabulations

--- front/src/components/AutoRegeneratedAvatar.vue
+++ front/src/components/AutoRegeneratedAvatar.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="avatar">
-        <v-runtime-template :template="template" />
+        <v-runtime-template :template="template"/>
     </div>
 
 </template>
@@ -12,32 +12,33 @@ import {defineComponent} from "vue";
 import type Configs from "holiday-avatar/dist/index";
 
 export default defineComponent({
-    props: ['avatarConfig'],
-    watch: {
-        avatarConfig: function (newVal: Configs) {
-            this.regenerateAvatar(newVal);
-        }
-    },
-    data() {
-        const config = genConfig({bgColor: '#FFF', mouthType: 'laugh'})
-        return {
-            template: '<Avatar v-bind="{...config}"  />',
-            config,
-        }
-    },
-
-    components: {
-        // eslint-disable-next-line vue/no-unused-components
-        Avatar,
-        VRuntimeTemplate,
-    },
-    methods: {
-        regenerateAvatar(newConfig: any) {
-            console.log(newConfig)
-            this.config = genConfig({bgColor: newConfig.bgColor, hatColor: newConfig.hatColor, faceColor: newConfig.faceColor, hairColor: newConfig.hairColor, shirtColor: newConfig.shirtColor, hairColorRandom: newConfig.hairColorRandom, sex: newConfig.sex, earSize: newConfig.earSize, hatType: newConfig.hatType, eyeType: newConfig.eyeType, hairType: newConfig.hairType, noseType: newConfig.noseType, mouthType: newConfig.mouthType, shirtType: newConfig.shirtType, eyeBrowType: newConfig.eyeBrowType, glassesType: newConfig.glassesType, shape: newConfig.shape});
-            this.template = '<Avatar v-bind="{...config}"  />';
-        },
+  props: ['avatarConfig'],
+  watch: {
+    avatarConfig: function (newVal: Configs) {
+      console.log(newVal)
+      this.regenerateAvatar(newVal)
     }
+  },
+  data() {
+    const config = genConfig({bgColor: '#FFF', mouthType: 'laugh'})
+    return {
+      template: '<Avatar v-bind="{...config}"  />',
+      config,
+    }
+  },
+
+  components: {
+    // eslint-disable-next-line vue/no-unused-components
+    Avatar,
+    VRuntimeTemplate,
+  },
+  methods: {
+    regenerateAvatar(newConfig: any) {
+      console.log(newConfig)
+      this.config = newConfig
+      this.template = '<Avatar v-bind="{...config}"  />';
+    },
+  }
 })
 
 </script>
--- front/src/views/UpdateUserView.vue
+++ front/src/views/UpdateUserView.vue
@@ -1,13 +1,6 @@
 <template>
-    <div class="avatar">
-        <v-runtime-template :template="template" />
-    </div>
+    <AutoRegeneratedAvatar :avatar-config="config"></AutoRegeneratedAvatar>
 
-<!--    <select @change="regenerateAvatar('mouthType', $event.target.value)">-->
-<!--        <option value="laugh">Rire</option>-->
-<!--        <option value="smile">Sourire</option>-->
-<!--        <option value="peace">Normal</option>-->
-<!--    </select>-->
     <div v-for="(item, key) in avatarProperties" :key="key">
         <AvatarRadioSelector :avatar-property=item @regenerateAvatar="regenerateAvatar"></AvatarRadioSelector>
     </div>
@@ -15,221 +8,234 @@
 </template>
 
 <script lang="ts">
-import {Avatar, genConfig} from "holiday-avatar";
-import VRuntimeTemplate from "vue3-runtime-template";
+import {genConfig} from "holiday-avatar";
 import {defineComponent} from "vue";
 import AvatarRadioSelector from "@/components/AvatarRadioSelector.vue";
+import AutoRegeneratedAvatar from "@/components/AutoRegeneratedAvatar.vue";
 
 export default defineComponent({
-        data() {
-            const config = genConfig({bgColor: '#FFF', mouthType: 'laugh'})
-            return {
-                template: '<Avatar v-bind="{...config}"  />',
-                config,
-                avatarProperties: [
-                    {
-                        propNameFr: 'Type de bouche',
-                        propNameEn: 'mouthType',
-                        propNameSnakeCase: 'mouth-type',
-                        propValues: [
-                            {
-                                propValueEn: 'laugh',
-                                propValueFr: 'Rire',
-                            },
-                            {
-                                propValueEn: 'smile',
-                                propValueFr: 'Sourire',
-                            },
-                            {
-                                propValueEn: 'peace',
-                                propValueFr: 'Normal',
-                            }
-                        ],
-                        selectedValueEn: config.mouthType
-                    },
-                    {
-                        propNameFr: 'Genre',
-                        propNameEn: 'sex',
-                        propNameSnakeCase: 'sex',
-                        propValues: [
-                            {
-                                propValueEn: 'male',
-                                propValueFr: 'Homme',
-                            },
-                            {
-                                propValueEn: 'female',
-                                propValueFr: 'Femme',
-                            }
-                        ],
-                        selectedValueEn: config.sex
-                    },
-                    {
-                        propNameFr: 'Taille des oreilles',
-                        propNameEn: 'earSize',
-                        propNameSnakeCase: 'ear-size',
-                        propValues: [
-                            {
-                                propValueEn: 'small',
-                                propValueFr: 'Petites',
-                            },
-                            {
-                                propValueEn: 'big',
-                                propValueFr: 'Grandes',
-                            }
-                        ],
-                        selectedValueEn: config.earSize
-                    },
-                    {
-                        propNameFr: 'Type des yeux',
-                        propNameEn: 'eyeType',
-                        propNameSnakeCase: 'eye-type',
-                        propValues: [
-                            {
-                                propValueEn: 'circle',
-                                propValueFr: 'Ronds',
-                            },
-                            {
-                                propValueEn: 'oval',
-                                propValueFr: 'Ovals',
-                            },
-                            {
-                                propValueEn: 'smile',
-                                propValueFr: 'Plissés',
-                            }
-                        ],
-                        selectedValueEn: config.eyeType
-                    },
-                    {
-                        propNameFr: 'Type de cheveux',
-                        propNameEn: 'hairType',
-                        propNameSnakeCase: 'hair-type',
-                        propValues: [
-                            {
-                                propValueEn: 'normal',
-                                propValueFr: 'Normals',
-                            },
-                            {
-                                propValueEn: 'thick',
-                                propValueFr: 'Epais',
-                            },
-                            {
-                                propValueEn: 'mohawk',
-                                propValueFr: 'Crête',
-                            },
-                            {
-                                propValueEn: 'femaleLong',
-                                propValueFr: 'Très longs',
-                            },
-                            {
-                                propValueEn: 'femaleShort',
-                                propValueFr: 'Longs',
-                            }
-                        ],
-                        selectedValueEn: config.hairType
-                    },
-                    {
-                        propNameFr: 'Type de chapeau',
-                        propNameEn: 'hatType',
-                        propNameSnakeCase: 'hat-type',
-                        propValues: [
-                            {
-                                propValueEn: 'none',
-                                propValueFr: 'Aucun',
-                            },
-                            {
-                                propValueEn: 'beanie',
-                                propValueFr: 'Bonnet',
-                            },
-                            {
-                                propValueEn: 'turban',
-                                propValueFr: 'Turban',
-                            }
-                        ],
-                        selectedValueEn: config.hatType
-                    },
-                    {
-                        propNameFr: 'Type de nez',
-                        propNameEn: 'noseType',
-                        propNameSnakeCase: 'nose-type',
-                        propValues: [
-                            {
-                                propValueEn: 'short',
-                                propValueFr: 'Court',
-                            },
-                            {
-                                propValueEn: 'long',
-                                propValueFr: 'Long',
-                            },
-                            {
-                                propValueEn: 'round',
-                                propValueFr: 'Rond',
-                            }
-                        ],
-                        selectedValueEn: config.noseType
-                    },
-                    {
-                        propNameFr: 'Type de haut',
-                        propNameEn: 'shirtType',
-                        propNameSnakeCase: 'shirt-type',
-                        propValues: [
-                            {
-                                propValueEn: 'hoody',
-                                propValueFr: 'Sweat',
-                            },
-                            {
-                                propValueEn: 'short',
-                                propValueFr: 'T-shirt',
-                            },
-                            {
-                                propValueEn: 'polo',
-                                propValueFr: 'Polo',
-                            }
-                        ],
-                        selectedValueEn: config.shirtType
-                    },
-                    {
-                        propNameFr: 'Type de lunettes',
-                        propNameEn: 'glassesType',
-                        propNameSnakeCase: 'glasses-type',
-                        propValues: [
-                            {
-                                propValueEn: 'none',
-                                propValueFr: 'Aucunes',
-                            },
-                            {
-                                propValueEn: 'round',
-                                propValueFr: 'Rondes',
-                            },
-                            {
-                                propValueEn: 'square',
-                                propValueFr: 'Carrées',
-                            }
-                        ],
-                        selectedValueEn: config.glassesType
-                    },
-
-                ],
-            }
-        },
-
-        components: {
-            AvatarRadioSelector,
-            // eslint-disable-next-line vue/no-unused-components
-            Avatar,
-            VRuntimeTemplate,
-        },
-        methods: {
-            regenerateAvatar(parameter: string, value: any) {
-                console.log(parameter, value)
-                const oldConfig = this.config;
-                if (parameter in oldConfig) {
-                    (oldConfig as {[index:string]: any})[parameter] = value;
-                }
-                console.log(oldConfig)
-                this.config = genConfig({bgColor: oldConfig.bgColor, hatColor: oldConfig.hatColor, faceColor: oldConfig.faceColor, hairColor: oldConfig.hairColor, shirtColor: oldConfig.shirtColor, hairColorRandom: oldConfig.hairColorRandom, sex: oldConfig.sex, earSize: oldConfig.earSize, hatType: oldConfig.hatType, eyeType: oldConfig.eyeType, hairType: oldConfig.hairType, noseType: oldConfig.noseType, mouthType: oldConfig.mouthType, shirtType: oldConfig.shirtType, eyeBrowType: oldConfig.eyeBrowType, glassesType: oldConfig.glassesType, shape: oldConfig.shape});
-                this.template = '<Avatar v-bind="{...config}"  />';
+  data() {
+    const config = genConfig({bgColor: '#FFF', mouthType: 'laugh'})
+    return {
+      template: '<Avatar v-bind="{...config}"  />',
+      config,
+      avatarProperties: [
+        {
+          propNameFr: 'Type de bouche',
+          propNameEn: 'mouthType',
+          propNameSnakeCase: 'mouth-type',
+          propValues: [
+            {
+              propValueEn: 'laugh',
+              propValueFr: 'Rire',
             },
-        }
+            {
+              propValueEn: 'smile',
+              propValueFr: 'Sourire',
+            },
+            {
+              propValueEn: 'peace',
+              propValueFr: 'Normal',
+            }
+          ],
+          selectedValueEn: config.mouthType
+        },
+        {
+          propNameFr: 'Genre',
+          propNameEn: 'sex',
+          propNameSnakeCase: 'sex',
+          propValues: [
+            {
+              propValueEn: 'male',
+              propValueFr: 'Homme',
+            },
+            {
+              propValueEn: 'female',
+              propValueFr: 'Femme',
+            }
+          ],
+          selectedValueEn: config.sex
+        },
+        {
+          propNameFr: 'Taille des oreilles',
+          propNameEn: 'earSize',
+          propNameSnakeCase: 'ear-size',
+          propValues: [
+            {
+              propValueEn: 'small',
+              propValueFr: 'Petites',
+            },
+            {
+              propValueEn: 'big',
+              propValueFr: 'Grandes',
+            }
+          ],
+          selectedValueEn: config.earSize
+        },
+        {
+          propNameFr: 'Type des yeux',
+          propNameEn: 'eyeType',
+          propNameSnakeCase: 'eye-type',
+          propValues: [
+            {
+              propValueEn: 'circle',
+              propValueFr: 'Ronds',
+            },
+            {
+              propValueEn: 'oval',
+              propValueFr: 'Ovals',
+            },
+            {
+              propValueEn: 'smile',
+              propValueFr: 'Plissés',
+            }
+          ],
+          selectedValueEn: config.eyeType
+        },
+        {
+          propNameFr: 'Type de cheveux',
+          propNameEn: 'hairType',
+          propNameSnakeCase: 'hair-type',
+          propValues: [
+            {
+              propValueEn: 'normal',
+              propValueFr: 'Normals',
+            },
+            {
+              propValueEn: 'thick',
+              propValueFr: 'Epais',
+            },
+            {
+              propValueEn: 'mohawk',
+              propValueFr: 'Crête',
+            },
+            {
+              propValueEn: 'femaleLong',
+              propValueFr: 'Très longs',
+            },
+            {
+              propValueEn: 'femaleShort',
+              propValueFr: 'Longs',
+            }
+          ],
+          selectedValueEn: config.hairType
+        },
+        {
+          propNameFr: 'Type de chapeau',
+          propNameEn: 'hatType',
+          propNameSnakeCase: 'hat-type',
+          propValues: [
+            {
+              propValueEn: 'none',
+              propValueFr: 'Aucun',
+            },
+            {
+              propValueEn: 'beanie',
+              propValueFr: 'Bonnet',
+            },
+            {
+              propValueEn: 'turban',
+              propValueFr: 'Turban',
+            }
+          ],
+          selectedValueEn: config.hatType
+        },
+        {
+          propNameFr: 'Type de nez',
+          propNameEn: 'noseType',
+          propNameSnakeCase: 'nose-type',
+          propValues: [
+            {
+              propValueEn: 'short',
+              propValueFr: 'Court',
+            },
+            {
+              propValueEn: 'long',
+              propValueFr: 'Long',
+            },
+            {
+              propValueEn: 'round',
+              propValueFr: 'Rond',
+            }
+          ],
+          selectedValueEn: config.noseType
+        },
+        {
+          propNameFr: 'Type de haut',
+          propNameEn: 'shirtType',
+          propNameSnakeCase: 'shirt-type',
+          propValues: [
+            {
+              propValueEn: 'hoody',
+              propValueFr: 'Sweat',
+            },
+            {
+              propValueEn: 'short',
+              propValueFr: 'T-shirt',
+            },
+            {
+              propValueEn: 'polo',
+              propValueFr: 'Polo',
+            }
+          ],
+          selectedValueEn: config.shirtType
+        },
+        {
+          propNameFr: 'Type de lunettes',
+          propNameEn: 'glassesType',
+          propNameSnakeCase: 'glasses-type',
+          propValues: [
+            {
+              propValueEn: 'none',
+              propValueFr: 'Aucunes',
+            },
+            {
+              propValueEn: 'round',
+              propValueFr: 'Rondes',
+            },
+            {
+              propValueEn: 'square',
+              propValueFr: 'Carrées',
+            }
+          ],
+          selectedValueEn: config.glassesType
+        },
+
+      ],
+    }
+  },
+
+  components: {
+    AutoRegeneratedAvatar,
+    AvatarRadioSelector,
+  },
+  methods: {
+    regenerateAvatar(parameter: string, value: any) {
+      if (parameter in this.config) {
+        (this.config as { [index: string]: any })[parameter] = value;
+      }
+      this.config = genConfig({
+        bgColor: this.config.bgColor,
+        hatColor: this.config.hatColor,
+        faceColor: this.config.faceColor,
+        hairColor: this.config.hairColor,
+        shirtColor: this.config.shirtColor,
+        hairColorRandom: this.config.hairColorRandom,
+        sex: this.config.sex,
+        earSize: this.config.earSize,
+        hatType: this.config.hatType,
+        eyeType: this.config.eyeType,
+        hairType: this.config.hairType,
+        noseType: this.config.noseType,
+        mouthType: this.config.mouthType,
+        shirtType: this.config.shirtType,
+        eyeBrowType: this.config.eyeBrowType,
+        glassesType: this.config.glassesType,
+        shape: this.config.shape
+      });
+
+    },
+  }
 })
 
 </script>
