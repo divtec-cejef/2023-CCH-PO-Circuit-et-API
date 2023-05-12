74211a463b77009ecb1923f817b8bee9cfa99406
chl0e-rnd <94692954+chl0e-rnd@users.noreply.github.com>
2023-05-12 15:46:58 +0200

    Ajout d'un alt

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -5,9 +5,7 @@
         <p>C'est par ici !</p>
     </div>
 
-    <img class="qr-code" src="../assets/img/qrCode.gif" alt="">
-
-
+    <img class="qr-code" src="../assets/img/qrCode.gif" alt="Animation qr code">
 </template>
 
 <script setup lang="ts">
