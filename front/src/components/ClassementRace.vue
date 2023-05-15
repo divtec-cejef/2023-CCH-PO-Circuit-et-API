fb8ff088b71525b55ca2cf582604fd5d4ec640cf
dindin-nibnib <mail@dindin.ch>
2023-05-15 13:35:13 +0200

    Added websocket connection on front

--- front/src/components/ClassementRace.vue
+++ front/src/components/ClassementRace.vue
@@ -1,20 +1,20 @@
 <template>
-  <ClassementElement v-for="(race, key) in listRace" :key="key" :avatar="race.car.avatar.image" :rank="key + 1"
-    :pseudo="race.car.pseudo" :time="race.total_time" />
-  <ClassementElement v-for="(race, key) in listRace" :key="key" :avatar="race.car.avatar.image" :rank="key + 1"
-    :pseudo="race.car.pseudo" :time="race.total_time" />
-  <ClassementElement v-for="(race, key) in listRace" :key="key" :avatar="race.car.avatar.image" :rank="key + 1"
-    :pseudo="race.car.pseudo" :time="race.total_time" />
-  <ClassementElement v-for="(race, key) in listRace" :key="key" :avatar="race.car.avatar.image" :rank="key + 1"
-    :pseudo="race.car.pseudo" :time="race.total_time" />
-  <ClassementElement v-for="(race, key) in listRace" :key="key" :avatar="race.car.avatar.image" :rank="key + 1"
-    :pseudo="race.car.pseudo" :time="race.total_time" />
-  <ClassementElement v-for="(race, key) in listRace" :key="key" :avatar="race.car.avatar.image" :rank="key + 1"
-    :pseudo="race.car.pseudo" :time="race.total_time" />
-  <ClassementElement v-for="(race, key) in listRace" :key="key" :avatar="race.car.avatar.image" :rank="key + 1"
-    :pseudo="race.car.pseudo" :time="race.total_time" />
-  <ClassementElement v-for="(race, key) in listRace" :key="key" :avatar="race.car.avatar.image" :rank="key + 1"
-    :pseudo="race.car.pseudo" :time="race.total_time" />
+    <ClassementElement v-for="(race, key) in listRace" :key="key" :avatar="race.car.avatar" :rank="key + 1"
+                       :pseudo="race.car.pseudo" :time="new Date(race.total_time)"/>
+    <ClassementElement v-for="(race, key) in listRace" :key="key" :avatar="race.car.avatar" :rank="key + 1"
+                       :pseudo="race.car.pseudo" :time="new Date(race.total_time)"/>
+    <ClassementElement v-for="(race, key) in listRace" :key="key" :avatar="race.car.avatar" :rank="key + 1"
+                       :pseudo="race.car.pseudo" :time="new Date(race.total_time)"/>
+    <ClassementElement v-for="(race, key) in listRace" :key="key" :avatar="race.car.avatar" :rank="key + 1"
+                       :pseudo="race.car.pseudo" :time="new Date(race.total_time)"/>
+    <ClassementElement v-for="(race, key) in listRace" :key="key" :avatar="race.car.avatar" :rank="key + 1"
+                       :pseudo="race.car.pseudo" :time="new Date(race.total_time)"/>
+    <ClassementElement v-for="(race, key) in listRace" :key="key" :avatar="race.car.avatar" :rank="key + 1"
+                       :pseudo="race.car.pseudo" :time="new Date(race.total_time)"/>
+    <ClassementElement v-for="(race, key) in listRace" :key="key" :avatar="race.car.avatar" :rank="key + 1"
+                       :pseudo="race.car.pseudo" :time="new Date(race.total_time)"/>
+    <ClassementElement v-for="(race, key) in listRace" :key="key" :avatar="race.car.avatar" :rank="key + 1"
+                       :pseudo="race.car.pseudo" :time="new Date(race.total_time)"/>
 </template>
 
 <script setup lang="ts">
@@ -23,21 +23,12 @@ import { websocket } from '@/models/api';
 import { ref, onUnmounted } from 'vue';
 import type { models } from '@/models/api';
 
-const listRace = ref<models.raceObject[]>([]);
+const listRace = ref<models.rankingData>();
 
 const socket = new websocket();
 
 socket.onRankingRecieved((data) => {
-
-  listRace.value = data.map((d) => {
-    return {
-      car: d.car,
-      // eslint-disable-next-line camelcase
-      id_race: d.id_race,
-      // eslint-disable-next-line camelcase
-      total_time: new Date(d.total_time),
-    };
-  });
+  listRace.value = data;
 });
 
 onUnmounted(() => {
