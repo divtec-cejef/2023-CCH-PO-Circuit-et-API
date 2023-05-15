3bf6ec9ebd34e99ded53d6f0268e99e96298b01f
chl0e-rnd <94692954+chl0e-rnd@users.noreply.github.com>
2023-05-15 13:14:10 +0200

    Add scroll to list race

--- front/src/components/TableListTime.vue
+++ front/src/components/TableListTime.vue
@@ -1,56 +1,68 @@
 <template>
-    <table>
-        <tr>
-            <th>N°</th>
-            <th>Heure</th>
-            <th>Vitesse</th>
-            <th>Temps</th>
-            <th>Vidéo</th>
-        </tr>
-        <tr v-for="(race, key) in props.carUser?.sortListByOrderHour()" :key="key">
-            <td>{{ props.carUser.getNumRace(race).valueOf() }}</td>
-            <td>{{ race.formatHour() }}</td>
-            <td>33</td>
-            <td>{{ race.formatTime(race.totalTime) }}</td>
-            <td><img class="video" src="../assets/img/film.png" alt="Icon de film pour visionner la vidéo de la course">
-            </td>
-        </tr>
-    </table>
+    <div>
+        <table>
+            <tr>
+                <th>N°</th>
+                <th>Heure</th>
+                <th>Vitesse</th>
+                <th>Temps</th>
+                <th>Vidéo</th>
+            </tr>
+            <tr v-for="(race, key) in props.carUser?.sortListByOrderHour()" :key="key">
+                <td>{{ props.carUser.getNumRace(race).valueOf() }}</td>
+                <td>{{ race.formatHour() }}</td>
+                <td>33</td>
+                <td>{{ race.formatTime(race.totalTime) }}</td>
+                <td><img class="video" src="../assets/img/film.png"
+                         alt="Icon de film pour visionner la vidéo de la course">
+                </td>
+            </tr>
+        </table>
+    </div>
 </template>
 
 <script setup lang="ts">
 import type Car from '@/models/car';
 
 const props = defineProps<{
-    carUser: Car;
+  carUser: Car;
 }>();
 
 </script>
 
 <style scoped lang="scss">
-table {
-  text-align: center;
+div {
+    overflow-y: scroll;
+    max-height: 400px;
+    display: flex;
+    justify-content: end;
+    padding: 0 10px;
 
-  tr td:nth-child(4) {
-    font-family: 'Digital-7 Mono', sans-serif;
-    font-size: 22px;
-    width: 45px;
-  }
+    table {
+        text-align: center;
 
-  td {
-    margin-right: 5px;
-  }
+        tr td:nth-child(4) {
+            font-family: 'Digital-7 Mono', sans-serif;
+            font-size: 22px;
+            width: 45px;
+        }
 
-  th {
-    text-align: left;
-  }
+        td {
+            margin-right: 5px;
+        }
 
-  img.video {
-    width: 25px;
-  }
+        th {
+            text-align: left;
+        }
 
-  img.flag-start {
-    width: 18px;
-  }
+        img.video {
+            width: 25px;
+        }
+
+        img.flag-start {
+            width: 18px;
+        }
+    }
 }
+
 </style>
