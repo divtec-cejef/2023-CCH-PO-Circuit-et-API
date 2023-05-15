6a74706865d5172593219d7e7219a3b0748cc3d4
dindin-nibnib <mail@dindin.ch>
2023-05-15 11:25:48 +0200

    added port environnement on api

--- api/index.ts
+++ api/index.ts
@@ -2,12 +2,15 @@ import expressServer from './clients/express';
 import sioServer from './clients/socketio';
 import prisma from './clients/prismadb';
 import http from 'http';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const server = http.createServer(expressServer);
 sioServer.attach(server);
 expressServer.set('socketio', sioServer);
 expressServer.set('prismadb', prisma);
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(process.env.PORT, () => {
+  console.log(`listening on *:${process.env.PORT}`);
 });
