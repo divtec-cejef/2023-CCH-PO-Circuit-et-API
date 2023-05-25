0abdb1420e351b5de18990a9489634b7e4436ebb
chl0e-rnd <94692954+chl0e-rnd@users.noreply.github.com>
2023-05-25 16:09:35 +0200

    Add scan page

---
+++ front/src/views/ActivityAdminView.vue
@@ -0,0 +1,29 @@
+<template>
+
+    <div class="activity-list">
+        <activity-admin @click="openScan"/>
+        <activity-admin />
+        <activity-admin />
+        <activity-admin />
+        <activity-admin />
+    </div>
+</template>
+
+<script setup lang="ts">
+import ActivityAdmin from '@/components/ActivityAdmin.vue';
+import router from '@/router';
+import { ref } from 'vue';
+
+const idActivity = ref(0);
+
+function openScan() {
+  router.push({ path: '/admin/scan', query: { idActivity: idActivity.value.toString() } });
+}
+</script>
+
+<style scoped lang="scss">
+
+div.activity-list div {
+    margin: 20px 0;
+}
+</style>
---
+++ front/src/views/ScannActivityView.vue
@@ -0,0 +1,16 @@
+<template>
+    <QrcodeStream @decode="(r) => result = r"  />
+
+    <div>{{ result }}</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { QrcodeStream } from 'vue-qrcode-reader/src';
+
+const result = ref('');
+</script>
+
+<style scoped lang="scss">
+
+</style>
