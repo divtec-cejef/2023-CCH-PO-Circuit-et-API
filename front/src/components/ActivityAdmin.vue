292983cac5d0b8408d4173c5bd815eb36fbf89f5
chl0e-rnd <94692954+chl0e-rnd@users.noreply.github.com>
2023-05-25 16:08:10 +0200

    Create one composant for activity

---
+++ front/src/components/ActivityAdmin.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="activity">
+slkdflskdjf
+</div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+
+.activity {
+  padding: 20px 15px;
+    box-shadow: rgba(100, 100, 111, 0.2) 0 7px 29px 0;
+    width: 100%;
+    border-radius: 15px;
+    text-align: center;
+}
+</style>
