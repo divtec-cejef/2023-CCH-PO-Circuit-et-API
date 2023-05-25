42c4f5ed502427e0767db6526c4fc5fa75965ac9
Tobias Glauser <tobiasglauser2006@gmail.com>
2023-05-25 14:55:09 +0200

    Added route post on realise

---
+++ api/routes/realise/post.ts
@@ -0,0 +1,31 @@
+import { routeHandler } from '../../models';
+import { checkStructureOrThrow } from 'check-structure';
+
+declare type realisedActivityRequest = {
+  id_activity: number,
+  id_car: number,
+  date_time: string,
+}
+export const route: routeHandler<null, unknown, realisedActivityRequest> = async (req, res) => {
+  const realisedActivity = req.body;
+
+  // Vérification de la structure de la requête
+  try {
+    checkStructureOrThrow(realisedActivity, {
+      id_activity: Number,
+      id_car: Number,
+      date_time: String
+    });
+  } catch (e) {
+    if (typeof e === 'string') {
+      res.status(400).json({ error: e });
+    } else if (e instanceof Error) {
+      res.status(400).json({ error: e.message });
+    } else {
+      res.status(400).send();
+    }
+  }
+
+  // Vérification de l'existence de l'activité
+
+};
