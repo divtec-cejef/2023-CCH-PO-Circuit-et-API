875965b9b7418e384d4723709e5f833064bf55e2
dindin-nibnib <mail@dindin.ch>
2023-05-26 13:40:47 +0200

    added testing section

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,7 @@ import buildSioServer from './clients/socketio';
 import prisma from './clients/prismadb';
 import http from 'http';
 import dotenv from 'dotenv';
+import cjs from 'crypto-js';
 
 dotenv.config();
 
@@ -10,6 +11,28 @@ const server = http.createServer(expressServer);
 
 expressServer.set('socketio', buildSioServer(server));
 expressServer.set('prismadb', prisma);
+
+if (process.env.CONTEXT === 'testing') {
+  (async () => {
+    await prisma.section.create({
+      data: {
+        label: 'test',
+        password: cjs.SHA256('Admlocal1').toString()
+      }
+    });
+
+    await prisma.activity.create({
+      data: {
+        label: 'tester',
+        section: {
+          connect: {
+            label: 'test'
+          }
+        }
+      }
+    });
+  })();
+}
 
 server.listen(process.env.PORT, () => {
   console.log(`listening on *:${process.env.PORT}`);
