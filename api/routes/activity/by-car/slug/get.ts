7a9d01345edb9c20ca2c8f7d995af0174b1806c7
Tobias Glauser <tobiasglauser2006@gmail.com>
2023-05-26 14:19:56 +0200

    Added comments

--- api/routes/activity/by-car/slug/get.ts
+++ api/routes/activity/by-car/slug/get.ts
@@ -11,17 +11,20 @@ import { getActivitiesByCarId } from '../../../../services/activity/implementati
 const route: routeHandler<{ slug: string; }> = async (req, res) => {
   const id = parseInt(req.params.slug);
 
+  // vérifie la validité de l'id
   if (typeof id === null || isNaN(id)) {
     res.status(400).json({ error: 'Invalid id' });
 
     return;
   }
 
+  // vérifie l'existence de la voiture
   if (await getCarById(id) === null) {
     res.status(404).json({ error: 'Car not found' });
     return;
   }
 
+  // retourne les activités de la voiture
   res.json(await getActivitiesByCarId(id));
 };
 export default route;
