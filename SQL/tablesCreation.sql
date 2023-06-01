a0c590b6f07e1d586dd9eb68dd53ddfa7095658a
dråfølin <mail@dindin.ch>
2023-06-01 13:18:57 +0200

    Added not null on required fields

--- SQL/tablesCreation.sql
+++ SQL/tablesCreation.sql
@@ -4,10 +4,10 @@ DROP TABLE IF EXISTS car CASCADE;
 CREATE TABLE car
 (
     id_car   SERIAL,
-    password VARCHAR(50),
-    query_id VARCHAR(100) UNIQUE,
-    pseudo   VARCHAR(50),
-    avatar   json,
+    password VARCHAR(50) NOT NULL,
+    query_id VARCHAR(100) NOT NULL UNIQUE,
+    pseudo   VARCHAR(50) NOT NULL,
+    avatar   json NOT NULL,
     PRIMARY KEY (id_car)
 );
 
@@ -36,7 +36,7 @@ DROP TABLE IF EXISTS activity CASCADE;
 CREATE TABLE activity
 (
     id_activity SERIAL,
-    label       VARCHAR(50),
+    label       VARCHAR(50) NOT NULL,
     id_section  INTEGER NOT NULL,
     PRIMARY KEY (id_activity),
     FOREIGN KEY (id_section) REFERENCES section (id_section)
@@ -45,9 +45,9 @@ CREATE TABLE activity
 DROP TABLE IF EXISTS realise CASCADE;
 CREATE TABLE realise
 (
-    id_car      SERIAL,
-    id_activity INTEGER,
-    date_time   TIMESTAMP,
+    id_car      SERIAL NOT NULL,
+    id_activity SERIAL NOT NULL,
+    date_time   TIMESTAMP NOT NULL,
     PRIMARY KEY (id_car, id_activity),
     FOREIGN KEY (id_car) REFERENCES car (id_car) ON DELETE CASCADE,
     FOREIGN KEY (id_activity) REFERENCES activity (id_activity)
