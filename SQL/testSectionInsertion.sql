9cb95dc5ab39898793416258f39f9cbfe983196b
Tobias Glauser <tobiasglauser2006@gmail.com>
2023-06-01 14:52:20 +0200

    Added password hash instead of password in testSectionInsertion.sql

--- SQL/testSectionInsertion.sql
+++ SQL/testSectionInsertion.sql
@@ -1,5 +1,5 @@
 INSERT INTO section (label, password)
-VALUES ('test', 'Admlocal1');
+VALUES ('test', 'ec53d08aca4ee303c639a2568bf6898573b8670a172ebe23ef4dc0aa745cbb13');
 
 INSERT INTO activity (label, id_section)
 VALUES ('tester', (SELECT id_section FROM section WHERE label = 'test'));
