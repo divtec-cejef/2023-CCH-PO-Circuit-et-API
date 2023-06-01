01913156c39fa573a102486ef94a2e15782cc593
chl0e-rnd <94692954+chl0e-rnd@users.noreply.github.com>
2023-06-01 14:18:51 +0200

    Ajout de la liste d'activité dans pinia et donc suppression du namespace activity

--- front/src/stores/adminPost.ts
+++ front/src/stores/adminPost.ts
@@ -1,11 +1,31 @@
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
 import type { Ref } from 'vue';
+import type { models } from '@/models/interface';
+import api from '@/models/api';
 
 export const useAdminPostStore = defineStore('adminPost', () => {
   const sectionName: Ref<string> = ref('');
   const mdp: Ref<string> = ref('');
   const token : Ref<string> = ref('');
+  const listActivity : Ref<models.activity[]> = ref([]);
 
-  return { sectionName, mdp, token };
+  /**
+   * Initialisation des activités d'une section
+   * @param idSection Id de la section
+   */
+  async function initAllActivityOneSection(idSection: number)  {
+    const { json: dataActivity } = await api.getAllActivityOneSection(idSection);
+
+    dataActivity.forEach((activityTable : any) => {
+      listActivity.value.push({
+        idActivity: activityTable['id_activity'],
+        name: activityTable['label'],
+        idSection : activityTable['id_section']
+      });
+    });
+
+    return listActivity;
+  }
+  return { sectionName, mdp, token, listActivity, initAllActivityOneSection };
 });
