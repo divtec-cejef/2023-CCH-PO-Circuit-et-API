5b8265800470de3a95acfbe04ce5e6af7fad82d7
Tobias Glauser <tobiasglauser2006@gmail.com>
2023-06-01 14:41:54 +0200

    Correct label activity from test to tester

--- SQL/testSectionInsertion.sql
+++ SQL/testSectionInsertion.sql
@@ -2,4 +2,4 @@ INSERT INTO section (label, password)
 VALUES ('test', 'Admlocal1');
 
 INSERT INTO activity (label, id_section)
-VALUES ('test', (SELECT id_section FROM section WHERE label = 'test'));
+VALUES ('tester', (SELECT id_section FROM section WHERE label = 'test'));
