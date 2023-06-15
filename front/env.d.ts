feab6e4fcd36ed0e9d73d062531beb8557819c07
dråfølin <mail@dindin.ch>
2023-06-15 14:29:35 +0200

    added vue for type-checking

--- front/env.d.ts
+++ front/env.d.ts
@@ -2,4 +2,9 @@ declare module '*.glb' {
     const content: string;
     export default content;
 }
+
+declare module '*.vue' {
+    const component;
+    export default component;
+}
 /// <reference types="vite/client" />
