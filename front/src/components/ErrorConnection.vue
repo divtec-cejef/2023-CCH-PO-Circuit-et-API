42aff152b6d2029922aa0ddd3bde262be49f7a8f
chl0e-rnd <94692954+chl0e-rnd@users.noreply.github.com>
2023-06-16 13:09:30 +0200

    Add error connection component

---
+++ front/src/components/ErrorConnection.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="content-error">
+        <div class="error-no-co" >
+            <h2>Erreur</h2>
+            <p>Problème de connexion...</p>
+            <p>Ressayez.</p>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+
+#app div.error-no-co {
+    top: calc(50% - 75px);
+    left: calc(50% - 200px);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 400px;
+    text-align: center;
+
+    h2 {
+        width: 100%;
+        text-align: center;
+        font-size: 45px;
+    }
+}
+</style>
--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,25 +1,31 @@
 <template>
-    <div class="intro">
-        <h1>Bienvenue !</h1>
-        <p>Tu n'as pas encore scanné de voiture...</p>
-        <p>C'est par ici !</p>
-    </div>
+    <template v-if="!(getLocalStorage && userCar.car.idQuery == '')">
+        <div class="intro">
+            <h1>Bienvenue !</h1>
+            <p>Tu n'as pas encore scanné de voiture...</p>
+            <p>C'est par ici !</p>
+        </div>
 
-    <img class="qr-code" :src=qrCodeImg alt="Animation qr code">
+        <img class="qr-code" :src=qrCodeImg alt="Animation qr code"></template>
 
+    <ErrorConnection v-else></ErrorConnection>
 </template>
 
 <script setup lang="ts">
 import qrCodeImg from '../assets/img/qrCode.gif';
 import { useCarStore } from '@/stores/car';
 import router from '@/router';
+import ErrorConnection from '@/components/ErrorConnection.vue';
 
 //Test si un utilisateur est déjà enregistré
 const userCar = useCarStore();
-if(userCar.car.idCar != undefined && userCar.car.idCar != 0) {
+if(userCar.car.idQuery != undefined && userCar.car.idQuery != '') {
   router.push({ path: `/${userCar.car.idQuery}` });
 }
 
+const getLocalStorage = () => {
+  return localStorage.getItem('userCarId');
+};
 </script>
 
 <style scoped lang="scss">
