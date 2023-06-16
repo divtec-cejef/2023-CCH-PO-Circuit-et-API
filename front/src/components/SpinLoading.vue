a92729d9aa98094ab3003efc63db18a5f79df91c
chl0e-rnd <94692954+chl0e-rnd@users.noreply.github.com>
2023-06-16 10:49:56 +0200

    Add loading spin element

---
+++ front/src/components/SpinLoading.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="spin">
+        <half-circle-spinner
+                :size="60"
+                color="#0C345A"
+        />
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { HalfCircleSpinner } from 'epic-spinners';
+
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
