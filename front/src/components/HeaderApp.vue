5231e7d455869f53e9e1ccdb7519682db2f60363
chl0e-rnd <94692954+chl0e-rnd@users.noreply.github.com>
2023-06-20 13:34:28 +0200

    Ajout du lien de stage dans le menu

--- front/src/components/HeaderApp.vue
+++ front/src/components/HeaderApp.vue
@@ -19,8 +19,13 @@
             <li @click="clickMenu" v-if="isAdmin && !isNaN(adminPost.idSection)">
                 <RouterLink to="/admin">Admin</RouterLink>
             </li>
-            <li @click="clickMenu">
-                <RouterLink to="/apropos">A Propos</RouterLink>
+            <li @click="clickMenu" id="stage">
+                <a href="https://forms.office.com/Pages/ResponsePage.aspx?id=p6gkJM1-REK-fgRvoEMkIDWILil6JahCo6JdgNf5EXJUMVpKQjBWOFZDT0IzRzc0QlY4RUNQTFk5SCQlQCN0PWcu"
+                   target="_blank">
+                    <img src="../assets/img/contract.png"
+                         alt="Icon d'inscription à un stage">
+                    <p>Stage</p>
+                </a>
             </li>
         </ul>
     </nav>
@@ -51,6 +56,35 @@ nav ul {
   margin-top: 50px;
   padding: 0;
   list-style: none;
+
+  li#stage a {
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    justify-content: center;
+    background-color: var(--pink-divtec);
+    border: 2px solid var(--pink-divtec);
+    padding: 7px 10px;
+    border-radius: 30px;
+
+    p {
+      color: var(--white);
+      margin-left: 4px;
+    }
+
+    img {
+      width: 20px;
+    }
+  }
+
+  li#stage a:hover {
+    background-color: var(--white);
+
+    p {
+      color: var(--pink-divtec);
+      margin-left: 4px;
+    }
+  }
 
   li {
     margin-top: 12px;
