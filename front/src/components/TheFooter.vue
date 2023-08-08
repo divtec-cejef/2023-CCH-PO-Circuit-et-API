4d1d71a2fbd493cb45ff1c295fc47f7031aabc12
chl0e-rnd <94692954+chl0e-rnd@users.noreply.github.com>
2023-08-08 23:45:32 +0200

    Changement d'emplacement pour le css du footer pour correction de l'erreur

--- front/src/components/TheFooter.vue
+++ front/src/components/TheFooter.vue
@@ -1,20 +1,20 @@
 <template>
-      <p>EMT, Portes ouvertes 2023</p>
-      <div class="stage">Inscris toi à un stage <a
-                href="https://forms.office.com/Pages/ResponsePage.aspx?id=p6gkJM1-REK-fgRvoEMkIDWILil6JahCo6JdgNf5EXJUMVpKQjBWOFZDT0IzRzc0QlY4RUNQTFk5SCQlQCN0PWcu">
-            ici</a> !
-        </div>
-      <div>
-          <a target="_blank" href="https://www.instagram.com/emtporrentruy/">
-              <img :src=instaImg
-                   alt="Logo instagram"></a>
-          <a target="_blank" href="https://www.facebook.com/DivtecCEJEF/">
-              <img :src=facebookImg
-                   alt="Logo faceboook"></a>
-            <RouterLink to="apropos">
-                A Propos
-            </RouterLink>
-      </div>
+    <p class="emt">EMT, Portes ouvertes 2023</p>
+    <div class="stage">Inscris toi à un stage <a
+            href="https://forms.office.com/Pages/ResponsePage.aspx?id=p6gkJM1-REK-fgRvoEMkIDWILil6JahCo6JdgNf5EXJUMVpKQjBWOFZDT0IzRzc0QlY4RUNQTFk5SCQlQCN0PWcu">
+        ici</a> !
+    </div>
+    <div class="link">
+        <a target="_blank" href="https://www.instagram.com/emtporrentruy/">
+            <img :src=instaImg
+                 alt="Logo instagram"></a>
+        <a target="_blank" href="https://www.facebook.com/DivtecCEJEF/">
+            <img :src=facebookImg
+                 alt="Logo faceboook"></a>
+        <RouterLink to="apropos">
+            A Propos
+        </RouterLink>
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -23,78 +23,68 @@ import facebookImg from '../assets/img/facebook.webp';
 </script>
 
 <style scoped lang="scss">
-footer {
-  bottom: 0;
-  width: 100%;
-  height: 65px;
-  background-color: var(--gray);
-  color: var(--white);
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+
+p.emt {
+  text-align: left;
+}
+
+.emt, .stage, .link {
+  width: 275px;
   text-align: center;
-  padding: 0 35px;
+  font-size: 16px;
+}
 
-  p {
-    font-size: 16px;
+.footer p:nth-child(1) {
+  text-align: left;
+}
+
+div.stage {
+  display: none;
+
+  a {
+    color: var(--light-green);
+    font-weight: bold;
+    transition: 0.1s ease-in-out;
   }
 
-  > * {
-    width: 275px;
-    text-align: center;
-  }
+  a:hover {
+    color: var(--white);
+    transition: 0.1s ease-in-out;
 
-  p:nth-child(1) {
-    text-align: left;
-  }
-
-  div.stage {
-    display: none;
-
-    a {
-      color: var(--light-green);
-      font-weight: bold;
-      transition: 0.1s ease-in-out;
-    }
-
-    a:hover {
-      color: var(--white);
-      transition: 0.1s ease-in-out;
-
-    }
-  }
-
-  div:nth-child(3) {
-    align-items: center;
-    display: flex;
-    flex-direction: row;
-    justify-content: end;
-
-    img {
-      width: 25px;
-    }
-
-    img:nth-child(1) {
-      margin-right: 15px;
-      transition: 0.2s filter ease-in-out;
-    }
-
-    img:hover {
-      filter: grayscale(8%) brightness(80%) sepia(40%) hue-rotate(50deg) saturate(300%);
-      transition: 0.2s filter ease-in-out;
-
-    }
-
-    a {
-      color: var(--white);
-      font-weight: bold;
-      transition: all ease-in-out 0.2s;
-    }
-
-    a:hover {
-      color: var(--light-green);
-      transition: all ease-in-out 0.2s;
-    }
   }
 }
+
+div.link {
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: end;
+
+  img {
+    width: 25px;
+  }
+
+  img:nth-child(1) {
+    margin-right: 15px;
+    transition: 0.2s filter ease-in-out;
+  }
+
+  img:hover {
+    filter: grayscale(8%) brightness(80%) sepia(40%) hue-rotate(50deg) saturate(300%);
+    transition: 0.2s filter ease-in-out;
+
+  }
+
+  a {
+    color: var(--white);
+    font-weight: bold;
+    transition: all ease-in-out 0.2s;
+  }
+
+  a:hover {
+    color: var(--light-green);
+    transition: all ease-in-out 0.2s;
+  }
+}
+
 </style>
