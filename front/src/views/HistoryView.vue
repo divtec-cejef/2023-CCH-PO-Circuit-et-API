d952220d12d7941853e701a113a58338d288b389
Tobias Glauser <tobiasglauser2006@gmail.com>
2023-08-22 10:50:07 +0200

    added map on page and added zoom and pan possibilities

--- front/src/views/HistoryView.vue
+++ front/src/views/HistoryView.vue
@@ -1,10 +1,35 @@
 <template>
-    <div/>
+    <div class="container">
+        <div :ref="panzoomable">
+            <BonusMap></BonusMap>
+        </div>
+    </div>
+
 </template>
 
 <script setup lang="ts">
+import BonusMap from '@/components/BonusMap.vue';
+import panzoom from 'panzoom';
+import { ref } from 'vue';
+
+
+const panzoomable = v => {
+  panzoom(v, {
+    bounds: true,
+    boundsPadding: 0.6,
+    maxZoom: 5,
+    minZoom: 1
+  });
+
+};
 </script>
 
 <style scoped>
-
+.container {
+    width: 100%;
+    height: 100%;
+    position: relative;
+    overflow: hidden;
+    background-color: #fff;
+}
 </style>
