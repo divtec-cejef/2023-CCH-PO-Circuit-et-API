7bacffbf7da47996869c724e38947dbbd18ab43f
Tobias Glauser <tobiasglauser2006@gmail.com>
2023-08-23 10:14:49 +0200

    Label display half finished

--- front/src/views/HistoryView.vue
+++ front/src/views/HistoryView.vue
@@ -3,7 +3,7 @@
         <div :ref="panzoomable">
             <BonusMap :display-label="displayLabel" :hide-label="hideLabel" :activities="allActivities"></BonusMap>
         </div>
-        <div ref="label" class="labelActivity" :style="{left: divLeft, top: divTop, hidden: divHidden}">
+        <div ref="label" class="labelActivity" :style="{left: divLeft, top: divTop, display: divDisplay}">
             <p>{{currentActivity === null ? null : currentActivity['labelActivity']}}</p>
         </div>
     </div>
@@ -18,20 +18,26 @@ import { ref } from 'vue';
 const label = ref<HTMLDivElement>();
 const divLeft = ref('0');
 const divTop = ref('0');
-const divHidden = ref(true);
+const divDisplay = ref('none');
 
 
 const divHeight = 50;
 const divWidth = 250;
 
 let currentActivity = null;
+let zoomfactor = 1;
 
 const panzoomable = v => {
-  panzoom(v, {
+  let element = panzoom(v, {
     bounds: true,
     boundsPadding: 0.6,
     maxZoom: 5,
     minZoom: 1
+  });
+
+  element.on('transform', function() {
+    zoomfactor = element.getTransform().scale;
+    divDisplay.value = 'none';
   });
 };
 
@@ -72,37 +78,42 @@ const allActivities = [{
   posY: 20,
 }];
 
-function calculatePositionX(posx, dif) {
+function calculatePositionX(posx, dif, zoomfactor) {
+  // console.log(posx, window.innerWidth / 2);
   if (posx > window.innerWidth / 2) {
-    return 'calc(' + posx + 'px - ' + (dif + divWidth) + 'px)';
+    console.log(posx, (posx - (dif + divWidth)) * zoomfactor + 'px');
+    return (posx - (divWidth)) + 'px';
   } else {
-    return 'calc(' + posx + 'px + ' + dif + 'px)';
+    console.log(posx, (posx + dif) * zoomfactor + 'px');
+    return (posx) + 'px';
   }
 }
 
-function calculatePositionY(posy, dif) {
+function calculatePositionY(posy, dif, zoomfactor) {
   if (posy > window.innerHeight / 2) {
-    return 'calc(' + posy + 'px - ' + (10 + dif + divHeight) + 'px)';
+    console.log(posy, (posy - (10 + dif + divHeight)) * zoomfactor + 'px');
+    return (posy) + 'px';
   } else {
-    return 'calc(' + posy + 'px + ' + (10 + dif) + 'px)';
+    console.log(posy, (posy + (10 + dif)) * zoomfactor + 'px');
+    return (posy) + 'px';
   }
 }
 
 function displayLabel(posx, posy, activityLabel) {
-  console.log(posx, posy, activityLabel);
+  // console.log(posx, posy, activityLabel);
   for (let activity of allActivities) {
     if (activity.labelActivity === activityLabel) {
       currentActivity = activity;
     }
   }
 
-  divLeft.value = calculatePositionX(posx, 10);
-  divTop.value = calculatePositionY(posy, 10);
-  divHidden.value = false;
+  divLeft.value = calculatePositionX(posx, 10, zoomfactor);
+  divTop.value = calculatePositionY(posy, 10, zoomfactor);
+  divDisplay.value = 'block';
 }
 
 function hideLabel() {
-  divHidden.value = true;
+  divDisplay.value = 'none';
 }
 </script>
 
@@ -120,5 +131,6 @@ function hideLabel() {
     height: 50px;
     display: none;
     position: absolute;
+    background-color: #ffffff;
 }
 </style>
