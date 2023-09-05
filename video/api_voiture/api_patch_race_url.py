b963095ed6955d7860da738d7d8d7b2faf87194d
Tobias Glauser <tobiasglauser2006@gmail.com>
2023-09-05 14:39:42 +0200

    Modified to go with api

--- video/api_voiture/api_patch_race_url.py
+++ video/api_voiture/api_patch_race_url.py
@@ -4,17 +4,27 @@ import os
 import requests
 
 
-def patch_race_url(race_id: int, url: str):
+def patch_race_url(*args):
     """
     Patch l'url de la vidéo de la course
-    :param race_id: Id de la course
-    :param url: Url de la vidéo
+    :param args: Id de la course, Url de la vidéo
     :return: none
     """
-    response = requests.patch(os.environ['API_URL'] + "race/", headers={"Authorization": "Bearer " + str(api_token.get_token())}, json={"id": race_id, "url": url})
+    response = patch_api_url_race(*args)
     if response.status_code == 401:
-        response = requests.patch(os.environ['API_URL'] + "race/", headers={"Authorization": "Bearer " + str(api_token.get_new_token())}, json={"id": race_id, "url": url})
+        response = patch_api_url_race(*args)
 
     if response.status_code != 200:
         raise Exception("Cannot insert url")
 
+
+def patch_api_url_race(id_race: int, video_url: str):
+    """
+    Patch l'url de la vidéo de la course
+    :param id_race: Id de la course
+    :param video_url: Url de la vidéo
+    :return: none
+    """
+    return requests.patch(os.environ['API_URL'] + "race/",
+                          headers={"Authorization": "Bearer " + str(api_token.get_token())},
+                          json={"id_race": id_race, "video_url": video_url})
