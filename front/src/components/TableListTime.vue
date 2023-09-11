d17216b0b9e2c5de105458c525f4b462393c8c98
chl0e-rnd <94692954+chl0e-rnd@users.noreply.github.com>
2023-09-11 11:15:14 +0200

    Change hover style for the link

--- front/src/components/TableListTime.vue
+++ front/src/components/TableListTime.vue
@@ -9,11 +9,14 @@
                 <th><img :src=video alt="Image de video"></th>
             </tr>
             <tr v-for="(race, key) in usercar.car.sortListByOrderHour()" :key="key">
-                <td><NumberTime class="num-race" :number=usercar.car.getNumRace(race).valueOf().toString() color="var(--blue)"/></td>
+                <td>
+                    <NumberTime class="num-race" :number=usercar.car.getNumRace(race).valueOf().toString()
+                                color="var(--blue)"/>
+                </td>
                 <td>{{ race.formatHour() }}</td>
                 <td>33</td>
                 <td>{{ race.formatTime(race.totalTime) }}</td>
-                <td><a :href="race.videoUrl">
+                <td><a :href="race.videoUrl" target="_blank">
                     <img class="link dark-invert" :src=link
                          alt="Icon de lien pour visionner la vidéo de la course">
                 </a>
@@ -38,81 +41,97 @@ const usercar = useCarStore();
 
 <style scoped lang="scss">
 div {
-    overflow-y: scroll;
-    max-height: 400px;
-    display: flex;
-    justify-content: end;
-    padding: 0 10px;
+  overflow-y: scroll;
+  max-height: 400px;
+  display: flex;
+  justify-content: end;
+  padding: 0 10px;
 
-    table {
-        text-align: center;
-        border-collapse: collapse;
+  table {
+    text-align: center;
+    border-collapse: collapse;
 
-        tr td:nth-child(4) {
-            font-family: 'Digital-7 Mono', sans-serif;
-            font-size: 22px;
-            width: 45px;
-        }
-
-        tr:first-child {
-            td,
-            th {
-                border-top: 0;
-            }
-            th:first-child {
-                padding: 0;
-            }
-        }
-
-        tr:last-child {
-            td,
-            th {
-                border-bottom: 0;
-            }
-        }
-
-        tr {
-            th,
-            td {
-                border: 2px solid #e2e2e7;
-                padding: 5px 2px;
-            }
-
-            td {
-                margin-right: 5px;
-                img.link {
-                    width: 18px;
-                }
-                img.flag-start {
-                     width: 18px;
-                 }
-            }
-
-            th {
-                p {
-                    text-align: left;
-                    margin-left: 7px;
-                }
-                img {
-                    width: 30px;
-                    min-width: 30px;
-                }
-                padding: 5px 0 2px 0;
-                border-left: 0;
-                border-right: 0;
-            }
-
-            td:first-child,
-            th:first-child {
-                border-left: 0;
-            }
-
-            td:last-child,
-            th:last-child {
-                border-right: 0;
-            }
-        }
+    tr td:nth-child(4) {
+      font-family: 'Digital-7 Mono', sans-serif;
+      font-size: 22px;
+      width: 45px;
     }
+
+    tr:first-child {
+      td,
+      th {
+        border-top: 0;
+      }
+
+      th:first-child {
+        padding: 0;
+      }
+    }
+
+    tr:last-child {
+      td,
+      th {
+        border-bottom: 0;
+      }
+    }
+
+    tr {
+      th,
+      td {
+        border: 2px solid #e2e2e7;
+        padding: 5px 2px;
+      }
+
+      td {
+        margin-right: 5px;
+
+        img.link {
+          width: 18px;
+        }
+
+        img.flag-start {
+          width: 18px;
+        }
+      }
+
+      th {
+        p {
+          text-align: left;
+          margin-left: 7px;
+        }
+
+        img {
+          width: 30px;
+          min-width: 30px;
+        }
+
+        padding: 5px 0 2px 0;
+        border-left: 0;
+        border-right: 0;
+      }
+
+      td:first-child,
+      th:first-child {
+        border-left: 0;
+      }
+
+      td:last-child,
+      th:last-child {
+        border-right: 0;
+      }
+    }
+  }
+
+  a {
+    img:hover {
+      transition: all ease-in-out 0.2s;
+      opacity: 0.5;
+    }
+
+    img {
+      transition: all ease-in-out 0.2s;
+    }
+  }
 }
 
 </style>
