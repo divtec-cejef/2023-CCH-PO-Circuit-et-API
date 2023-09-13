de5c3b2cdca8f134d750dc2222b70ce6b3c845b1
chl0e-rnd <94692954+chl0e-rnd@users.noreply.github.com>
2023-09-13 16:54:26 +0200

    Ajout de la page pour l'inscrition de stage et iframe

--- front/src/views/StageView.vue
+++ front/src/views/StageView.vue
@@ -1,8 +1,8 @@
 <template>
     <iframe height="480px"
             src="https://forms.office.com/Pages/ResponsePage.aspx?id=p6gkJM1-REK-fgRvoEMkIDWILil6JahCo6JdgNf5EXJUMVpKQjBWOFZDT0IzRzc0QlY4RUNQTFk5SCQlQCN0PWcu&embed=true"
-            frameborder="0" marginwidth="0" marginheight="0" style="border: none; max-width:100%; max-height:100vh"
-            allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen></iframe>
+             style="border: none; max-width:100%; max-height:100vh"
+            allowfullscreen ></iframe>
 </template>
 
 <script setup lang="ts">
