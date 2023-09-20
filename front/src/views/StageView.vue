34ff3edce829c1c9b200548bd7ff2d0a9639d55d
Tobias Glauser <tobiasglauser2006@gmail.com>
2023-09-20 14:42:58 +0200

    Changed iframe parameters

--- front/src/views/StageView.vue
+++ front/src/views/StageView.vue
@@ -1,5 +1,5 @@
 <template>
-    <iframe height="480px"
+    <iframe height="480px" class="fullscreen"
             src="https://forms.office.com/Pages/ResponsePage.aspx?id=p6gkJM1-REK-fgRvoEMkIDWILil6JahCo6JdgNf5EXJUMVpKQjBWOFZDT0IzRzc0QlY4RUNQTFk5SCQlQCN0PWcu&embed=true"
              style="border: none; max-width:100%; max-height:100vh"
             allowfullscreen ></iframe>
@@ -12,7 +12,7 @@
 <style scoped lang="scss">
 
 iframe {
-    width: 100%;
-    height: calc(100vh - 240px);
+    margin-top: 100px;
+    height: calc(100vh - var(--height-screen-diff) - 85px);
 }
 </style>
