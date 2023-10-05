bc4ece4256c627663302b5ce4a0e43905e693a85
drafolin <derg@drafolin.ch>
2023-10-05 16:16:44 +0200

    fixed links in header and footer

--- front/src/components/TheFooter.vue
+++ front/src/components/TheFooter.vue
@@ -1,36 +1,36 @@
 <template>
     <p class="emt">EMT, Portes ouvertes 2023</p>
     <div class="stage">Inscris toi à un stage
-        <RouterLink to="stage">
+        <RouterLink to="/stage">
             ici
         </RouterLink> !
     </div>
     <div class="link">
         <template v-if="display !== 'legacy'">
-            <a target="_blank" href="https://www.instagram.com/emtporrentruy/">
+            <a href="https://www.instagram.com/emtporrentruy/" target="_blank">
                 <img :src=instaImg
                      alt="Logo instagram">
             </a>
-            <a target="_blank" href="https://www.facebook.com/DivtecCEJEF/">
+            <a href="https://www.facebook.com/DivtecCEJEF/" target="_blank">
                 <img :src=facebookImg
                      alt="Logo faceboook">
             </a>
         </template>
-        <RouterLink to="apropos">
+        <RouterLink to="/apropos">
             A Propos
         </RouterLink>
     </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
 import instaImg from '@/assets/img/instagram.webp';
 import facebookImg from '@/assets/img/facebook.webp';
-import { useLocalStorage } from '@vueuse/core';
+import {useLocalStorage} from '@vueuse/core';
 
 const display = useLocalStorage('display', 'modern');
 </script>
 
-<style scoped lang="scss">
+<style lang="scss" scoped>
 
 p.emt {
   text-align: left;
