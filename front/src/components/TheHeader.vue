1136b791cc7bc8282ccc0fb29275628b2389533e
NathanGenesta <nathan.genesta@divtec.ch>
2024-11-20 10:19:16 +0100

    Ajout formulaire stage

--- front/src/components/TheHeader.vue
+++ front/src/components/TheHeader.vue
@@ -30,7 +30,7 @@
                 <img :src="live" alt="Image de live pour le live">
             </li>
             <li @click="clickMenu">
-                <RouterLink to="/scanneurQR">Scanneur carte</RouterLink>
+                <RouterLink to="/scanneurQR">Scanneur bonus</RouterLink>
             </li>
             <li id="stage" @click="clickMenu">
                 <RouterLink to="/stage">
