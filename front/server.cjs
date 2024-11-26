c71542c839cf6e0b5879755591880a920fbca103
NathanGenesta <nathan.genesta@divtec.ch>
2024-11-26 10:46:55 +0100

    Déplacement de code

--- front/server.cjs
+++ front/server.cjs
@@ -13,11 +13,11 @@ app.all('*', (req, res) => {
     res.sendFile('index.html', {root: 'dist'});
 });
 
+app.listen(httpPort, () => {
+    console.log(`HTTP server listening on port ${httpPort}`);
+});
+
 app.get('/random-business', async (req, res) => {
     const randomBusiness = await db.query('SELECT * FROM business ORDER BY RAND() LIMIT 1');
     res.json(randomBusiness[0]); // Retourne une seule entreprise
   });
-
-app.listen(httpPort, () => {
-    console.log(`HTTP server listening on port ${httpPort}`);
-});
