337bf86e845240655e14ecd10798d3598e3194ac
Rédouane Drici <redouane.drici@divtec.ch>
2025-11-11 11:25:08 +0100

    correction api

--- api/services/race/getLastRace.ts
+++ api/services/race/getLastRace.ts
@@ -1,6 +1,9 @@
-export const getLastRace = async (): Promise<Race | null> => {
-    return await prisma.race.findFirst({
-        orderBy: { id_race: 'desc' },
-        include: { car: true }
-    });
+import prisma from '../../clients/prismadb';
+import { race } from '@prisma/client';
+
+export const getLastRace = async (): Promise<race | null> => {
+  return await prisma.race.findFirst({
+    orderBy: { id_race: 'desc' },
+    include: { car: true }
+  });
 };
