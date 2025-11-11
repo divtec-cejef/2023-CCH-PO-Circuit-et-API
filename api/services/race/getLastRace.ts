9292b0d963ec91891e352a78ce61da950aaba3fa
Rédouane Drici <redouane.drici@divtec.ch>
2025-11-11 11:26:53 +0100

    commit inital

--- api/services/race/getLastRace.ts
+++ api/services/race/getLastRace.ts
@@ -1,6 +1,7 @@
 import prisma from '../../clients/prismadb';
 import { race } from '@prisma/client';
 
+
 export const getLastRace = async (): Promise<race | null> => {
   return await prisma.race.findFirst({
     orderBy: { id_race: 'desc' },
